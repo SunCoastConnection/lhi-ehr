be28915d44129994d37342ef4dca2747ccb55cdf
Terry Hill <teryhill@yahoo.com>
2018-04-23 16:10:16 -0400

    Minor changes
    Minor changes

--- interface/main/tabs/templates/user_data_template.php
+++ interface/main/tabs/templates/user_data_template.php
@@ -13,23 +13,23 @@
 <link rel="stylesheet" type="text/css" href=".../../css/userdata.css"/>
 <script type="text/html" id="user-data-template">
     <!-- ko with: user -->
-        <ul id="userdata" class="nav navbar-nav navbar-right" title="<?php echo xla('Authorization group') ?>">
+        <nav class="nav navbar-nav navbar-right" style="margin-right: 10px !important;">
+        <p class="clearfix"></p>
+        <p class="clearfix"></p>
+        <ul class="list-unstyled" id="userdata">
+            <a> <span data-bind="click: logout"><b><?php echo xlt("Logout");?></b></span></a>
             <li class="dropdown" >
                 <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                <span class="glyphicon glyphicon-user"></span>
+                <span data-bind="text:fname"></span>&nbsp;<span data-bind="text:lname"></span>
                 </a>
                 <ul class="dropdown-menu">
-                    <li><span data-bind="text:fname"></span>&nbsp;<span data-bind="text:lname"></span></li>
-                    <li class="divider"></li>
-                    <?php if ($GLOBALS['development_flag'] ) { ?>
-                    <li data-bind="click: framesMode"><?php echo xlt("Frames Mode");?></li>
-                    <?php } ?>
                     <li data-bind="click: userPrefs"><?php echo xlt("User Preferences");?></li>
                     <li data-bind="click: changePassword"><?php echo xlt("Change Pass Phrase");?></li>
-                    <li data-bind="click: logout"><?php echo xlt("Logout");?></li>
+                    <!--<li data-bind="click: logout"><?php //echo xlt("Logout");?></li>-->
                 </ul>
             </li>
         </ul>
+   </nav>
 
     <!-- /ko -->
     <div id="messagesNotification" data-bind="click: unreadMessages">
